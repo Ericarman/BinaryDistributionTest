e98345e16e087e5b5f7d24c71ab1272e311cf848
Eric Hovhannisyan <Erichovhannisyan13@gmail.com>
2023-12-13 22:25:24 +0400

    Test internal file

---
+++ MyTour/MyInternalFile.swift
@@ -0,0 +1,18 @@
+//
+//  InternalFile.swift
+//  
+//
+//  Created by Eric Hovhannisyan on 13.12.23.
+//
+
+import Foundation
+
+final class InternalFile {
+    init() {
+        doStuff()
+    }
+    
+    func doStuff() {
+        print("Do stuff")
+    }
+}
--- MyTour/Test.swift
+++ MyTour/Test.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public final class Test {
-    init() {
+    public init() {
         test()
     }
     
